457536e987359af88cf395a0f4b1f1a6da43d3bf
AlexandraBancheva <sandriche90@gmail.com>
2021-07-19 21:06:36 +0300

    Add Category seeder

---
+++ TourismPlatform/Data/TourismPlatform.Data/Seeding/CategoriesSeeder.cs
@@ -0,0 +1,71 @@
+﻿namespace TourismPlatform.Data.Seeding
+{
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using TourismPlatform.Data.Models;
+
+    public class CategoriesSeeder : ISeeder
+    {
+        public async Task SeedAsync(ApplicationDbContext dbContext, IServiceProvider serviceProvider)
+        {
+            if (dbContext.Categories.Any())
+            {
+                return;
+            }
+
+            await dbContext.Categories.AddAsync(new Category
+            {
+                Name = "Културен туризъм",
+            });
+
+            await dbContext.Categories.AddAsync(new Category
+            {
+                Name = "Екологичен туризъм",
+            });
+
+            await dbContext.Categories.AddAsync(new Category
+            {
+                Name = "Морски туризъм",
+            });
+
+            await dbContext.Categories.AddAsync(new Category
+            {
+                Name = "Планински/ Ски туризъм",
+            });
+
+            await dbContext.Categories.AddAsync(new Category
+            {
+                Name = "Балнео, СПА и Уелнес",
+            });
+
+            await dbContext.Categories.AddAsync(new Category
+            {
+                Name = "Спортен/ Приключенски туризъм",
+            });
+
+            await dbContext.Categories.AddAsync(new Category
+            {
+                Name = "Селски туризъм",
+            });
+
+            await dbContext.Categories.AddAsync(new Category
+            {
+                Name = "Конгресен туризъм",
+            });
+
+            await dbContext.Categories.AddAsync(new Category
+            {
+                Name = "Къмпинг туризъм",
+            });
+
+            await dbContext.Categories.AddAsync(new Category
+            {
+                Name = "Винен/ Кулинарен туризъм",
+            });
+
+            await dbContext.SaveChangesAsync();
+        }
+    }
+}
